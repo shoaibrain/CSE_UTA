ebcd8212588b1293b810ecb0383d844547367ac3
Shoaib Rain <sxr5785@mavs.uta.edu>
2020-10-10 23:05:31 -0500

    Initial Commit

---
+++ CMakeFiles/1205_Hello_World.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1205_Hello_World.dir/1205_Hello_World.c.o"
+  "1205_Hello_World.pdb"
+  "1205_Hello_World"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1205_Hello_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
